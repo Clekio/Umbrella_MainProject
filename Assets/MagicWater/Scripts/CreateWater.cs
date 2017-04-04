b811012bfc4bc61cac0d348bfe4b286c18fc1501
MrPuga <cpgblog@gmail.com>
2017-04-04 21:21:23 +0200

    MagicWater

---
+++ Assets/MagicWater/Scripts/CreateWater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWater : MonoBehaviour {
+
+	public GameObject Particler;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Debug.Log ("ButtonDown");
+			Particler.SetActive (true);
+		}
+		if (Input.GetButtonUp ("Fire1")) {
+			Debug.Log ("ButtonUp");
+			Particler.SetActive (false);
+		}
+
+		Particler.transform.position = Input.mousePosition;
+			
+		
+	}
+}
---
+++ Assets/MagicWater/Scripts/WaterEffects.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffects : MonoBehaviour {
+
+	public bool barro;
+	public bool planta;
+	public bool fuego;
+	public float speed;
+
+	public Transform plantPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col){
+		if (col.tag == "DynamicParticle") {
+//			if (col.GetComponent <DynamicParticle> ().currentState == col.GetComponent <DynamicParticle> ().STATES.WATER) {
+				ApplyEffect ();
+//			}
+		}
+	}
+
+	void ApplyEffect (){
+		if (barro) {
+			EfectoBarro ();
+		}
+		if (planta) {
+			EfectoPlanta ();
+		}
+		if (fuego) {
+			EfectoFuego ();
+		}
+	}
+
+	void EfectoBarro (){
+		gameObject.GetComponent <Renderer> ().material.color = Color.grey;
+	}
+	void EfectoPlanta (){
+		gameObject.GetComponent <Renderer> ().material.color = Color.green;
+		if (transform.position != plantPosition.position){
+			transform.position = Vector2.MoveTowards (transform.position, plantPosition.position, speed);
+		}
+	}
+	void EfectoFuego (){
+		gameObject.GetComponent <Renderer> ().material.color = Color.yellow;
+	}
+
+}
