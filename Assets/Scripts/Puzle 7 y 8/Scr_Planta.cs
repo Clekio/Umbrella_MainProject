5707a1763d20881f102e26d38298e1138b22365b
VictorFernandez7 <victorfernandez95@hotmail.com>
2017-04-27 14:02:28 +0200

    Puzles 7 y 8

---
+++ Assets/Scripts/Puzle 7 y 8/Scr_Planta.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Planta : MonoBehaviour
+{
+    public GameObject generarObjeto;
+    public Vector3 spawn = new Vector3(112, 21, 0);
+
+    void Start()
+    {
+        Instantiate(generarObjeto, spawn, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Puzle 7 y 8/Spr_Caja.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spr_Caja : MonoBehaviour
+{
+    public GameObject caja;
+
+    void OnCollisionEnter2D(Collision2D suelo)
+    {
+        if (suelo.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("rgesafd");
+        }
+          //  Destroy(caja, 3);
+    }
+}
